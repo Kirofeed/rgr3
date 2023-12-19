983bdaba66332926051a2b344d3a94603484cb78
kirilldrozhzha <Drozhzhakirill@gmail.com>
2023-12-19 22:18:40 +0300

    done

---
+++ cmake-build-debug/CMakeFiles/Library.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Library.dir/lib.cpp.o"
+  "CMakeFiles/Library.dir/lib.cpp.o.d"
+  "Library"
+  "Library.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Library.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/rgr3.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rgr3.dir/main.cpp.o"
+  "CMakeFiles/rgr3.dir/main.cpp.o.d"
+  "rgr3"
+  "rgr3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rgr3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
